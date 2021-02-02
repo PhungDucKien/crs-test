71d49d9ff2775664d5d0d02e042bdece4ffb835e
PhungDucKien <kien.pd@altplus.com.vn>
2021-02-02 11:07:47 +0700

    Update file_with_secrets.py

--- file_with_secrets.py
+++ file_with_secrets.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # Will change this later.
 SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIGNhdXNlIGVub3VnaCBlbnRyb3B5'
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvd123WxkIGNhdXNlIGVub3VnaCBlbnRyb3B5'
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIxczxcGNhdXNlIGVub3VnaCBlbnRyb3B5'
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdW123zxczxkIGNhdXNlIGVub3VnaCBlbnRyb3B5'
+SUPER_SEECRET_VALUE = 'c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxccckIGNhdXNlIGVub3VnaCBlbnRyb3B5'
 VERY_SECRET_TOO = 'f6CGV4aMM9zedoh3OUNbSakBymo7yplB'  # pragma: allowlist secret
 
 
